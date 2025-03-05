0a1928ec47633e59dbce2b177019d651fcbc4b09
MartinSJosefsson <martin.s.josefsson@gmail.com>
2025-03-05 15:42:19 +0100

    Added variables

--- my-first-java-application/src/main/java/se/lexicon/Main.java
+++ my-first-java-application/src/main/java/se/lexicon/Main.java
@@ -1,7 +1,56 @@
 package se.lexicon;
 
+
 public class Main {
+
+    /* This is a multiline comment
+    it can span several lines
+     */
+
+    /**
+     * This is a Java Doc <b>(Documentary Comments)</b>
+     * ## Used to create documentation for **Java Codes**
+     */
+
+
+    // psvm + tab
     public static void main(String[] args) {
+        //sout + tab
         System.out.println("Hello, World!");
+        System.out.println("----------------");
+        System.out.println("Hello");
+        System.out.println("Martin");
+        System.out.println("----------------");
+
+        System.out.println("Hello again!");
+
+        System.out.println("----------------");
+        System.out.println("Speed");
+
+        //Variables
+        // [data type] [value name];
+        int speed;
+        speed = 100;
+
+        double salary = 5000.55;
+        char grade = 'A';
+        char specialChar ='\u2764';
+        boolean isActive = true;
+        boolean isActive = false;
+
+        long balance = 500000000000000L;
+        double area = 20.55;
+        int setNumber = 100;
+        int studentId = 5000;
+
+
+        System.out.println("Speed" + " " + speed);
+
+
+
     }
+
+    // Ctrl + Alt + L = reformat code
+
+
 }
